3a897124e3179282bad4493948558510686d7b77
fabio henrique da silva <hantarodev@gmailcom>
2019-06-11 00:48:52 -0300

    Initial commit

---
+++ app/src/main/java/hantaro/com/tourguideapp/Helper.java
@@ -0,0 +1,12 @@
+package hantaro.com.tourguideapp;
+
+
+import android.content.res.Resources;
+
+public class Helper {
+    public static final String RESTAURANTS = Resources.getSystem().getString(R.string.restaurant_label);
+    public static final String PARKS = Resources.getSystem().getString(R.string.parks_label);
+    public static final String HOTELS = Resources.getSystem().getString(R.string.hotels_label);
+    public static final String ATTRACTIONS = Resources.getSystem().getString(R.string.attractions_label);
+
+}
